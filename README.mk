d2352946821bb2ce5e91382ab1b119679c211f30
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-04-08 18:16:48 -0700

    included cli

---
+++ README.mk
@@ -0,0 +1,10 @@
+# Blackjack game project
+
+First project using Flask from first phase of NuCamp's backend development bootcamp.
+Requires user to register account and login with valid credentials before playing. User account and leaderboard data is stored in a PostgreSQL database.
+
+
+
+## Future improvements
+
+To include a python GUI, deploy to AWS/Azure
